be21784a1909c11d16d8d9eb1873e779702c2744
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-11 16:13:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+if speed<40 || speed>60
+  return true
+else
+  return false
 end
-
-
+end
+unsafe?(100)
 
 def not_safe?(speed)
-	
+  
+  speed > 60 ? true : false ||
+  speed < 40 ? true : false
+
 end
+not_safe?(50)
+
+
+ 
 	
 
 

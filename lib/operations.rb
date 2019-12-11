08af202972c9688129e6e5ecbafed87e2306f60a
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-11 16:20:32 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+if speed<40 || speed>60
+  return true
+else
+  return false
 end
-
-
+end
+unsafe?(100)
 
 def not_safe?(speed)
-	
+  
+  speed > 60 || speed < 40 ?  true : false
+
 end
+
+
+
+ 
 	
 
 
